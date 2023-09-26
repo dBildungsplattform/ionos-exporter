ec3ab458d098e58e67d8613cb65691bee126b3cd
ldahlke <lars.dahlke@capgemini.com>
2023-09-26 06:08:25 +0000

    Initial working version of ionos exporter

---
+++ internal/ionos.go
@@ -0,0 +1,108 @@
+package internal
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"sync"
+	"time"
+
+	ionoscloud "github.com/ionos-cloud/sdk-go/v6"
+)
+
+var (
+	CoresTotal       int32 = 0
+	RamTotal         int32 = 0
+	ServerTotal      int32 = 0
+	DataCenters      int32 = 0
+	IonosDatacenters       = make(map[string]IonosDCResources) //Key is the name of the datacenter
+	depth            int32 = 1                                 //Controls the detail depth of the response objects.
+)
+
+type IonosDCResources struct {
+	Cores   int32  // Amount of CPU cores in the whole DC, regardless whether it is a VM or Kubernetscluster
+	Ram     int32  // Amount of RAM in the whole DC, regardless whether it is a VM or Kubernetscluster
+	Servers int32  // Amount of servers in the whole DC
+	DCId    string // UUID od the datacenter
+}
+
+func CollectResources(m *sync.RWMutex, cycletime int32) {
+	configuration := ionoscloud.NewConfigurationFromEnv()
+	apiClient := ionoscloud.NewAPIClient(configuration)
+	for {
+		datacenters, resp, err := apiClient.DataCentersApi.DatacentersGet(context.Background()).Depth(depth).Execute()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error when calling `DataCentersApi.DatacentersGet``: %v\n", err)
+			fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", resp)
+			os.Exit(1)
+		}
+		for _, datacenter := range *datacenters.Items {
+			var (
+				coresTotalDC  int32 = 0
+				ramTotalDC    int32 = 0
+				serverTotalDC int32 = 0
+			)
+			servers, resp, err := apiClient.ServersApi.DatacentersServersGet(context.Background(), *datacenter.Id).Depth(depth).Execute()
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "Error when calling `ServersApi.DatacentersServersGet``: %v\n", err)
+				fmt.Fprintf(os.Stderr, "Full HTTP response: %v\n", resp)
+			}
+			serverTotalDC = int32(len(*servers.Items))
+			for _, server := range *servers.Items {
+				coresTotalDC += *server.Properties.Cores
+				ramTotalDC += *server.Properties.Ram
+			}
+			m.Lock()
+			IonosDatacenters[*datacenter.Properties.Name] = IonosDCResources{
+				DCId:    *datacenter.Id,
+				Cores:   coresTotalDC,
+				Ram:     ramTotalDC,
+				Servers: serverTotalDC,
+			}
+			m.Unlock()
+			CalculateDCTotals(m)
+		}
+		time.Sleep(time.Duration(cycletime) * time.Second)
+	}
+}
+func CalculateDCTotals(m *sync.RWMutex) {
+	var (
+		serverTotal      int32
+		ramTotal         int32
+		coresTotal       int32
+		datacentersTotal int32
+	)
+	m.RLock()
+	for _, dcResources := range IonosDatacenters {
+		serverTotal += dcResources.Servers
+		ramTotal += dcResources.Ram
+		coresTotal += dcResources.Cores
+	}
+	datacentersTotal = int32(len(IonosDatacenters))
+	m.RUnlock()
+	m.Lock()
+	ServerTotal = serverTotal
+	RamTotal = ramTotal
+	CoresTotal = coresTotal
+	DataCenters = datacentersTotal
+	m.Unlock()
+}
+func PrintDCResources(m *sync.RWMutex) {
+	m.RLock()
+	defer m.RUnlock()
+	for dcName, dcResources := range IonosDatacenters {
+		fmt.Fprintf(os.Stdout, "%s:\n    - UUID: %s\n", dcName, dcResources.DCId)
+		fmt.Fprintf(os.Stdout, "    - Servers: %d\n", dcResources.Servers)
+		fmt.Fprintf(os.Stdout, "    - Cores: %d\n", dcResources.Cores)
+		fmt.Fprintf(os.Stdout, "    - Ram: %d GB\n", dcResources.Ram/1024)
+	}
+}
+func PrintDCTotals(m *sync.RWMutex) {
+	m.RLock()
+	defer m.RUnlock()
+	log.Printf("Total - Datacenters: %d\n", DataCenters)
+	log.Printf("Total - Servers: %d\n", ServerTotal)
+	log.Printf("Total - Cores: %d\n", CoresTotal)
+	log.Printf("Total - Ram: %d GB\n", RamTotal/1024)
+}
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"ionos-exporter/internal"
+	"log"
+	"net/http"
+	"strconv"
+	"sync"
+
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var (
+	mutex           = &sync.RWMutex{} // Mutex to sync access to the Daatcenter map
+	exporterPort    string            // Port to be used for exposing the metrics
+	ionos_api_cycle int32             // Cycle time in seconds to query the IONOS API for changes, not th ePrometheus scraping intervall
+)
+
+func main() {
+	exporterPort = internal.GetEnv("IONOS_EXPORTER_PORT", "9100")
+	if cycletime, err := strconv.ParseInt(internal.GetEnv("IONOS_API_CYCLE", "900"), 10, 32); err != nil {
+		log.Fatal("Cannot convert IONOS_API_CYCLE to int")
+	} else {
+		ionos_api_cycle = int32(cycletime)
+	}
+	go internal.CollectResources(mutex, ionos_api_cycle)
+	internal.StartPrometheus(mutex)
+	http.Handle("/metrics", promhttp.Handler())
+	http.Handle("/healthcheck", http.HandlerFunc(internal.HealthCheck))
+	log.Fatal(http.ListenAndServe(":" + exporterPort, nil))
+}
